7a99db85b6b125b0c31954a23f2f0fa83917d473
Mauricio Cardoso <mauriciolacardoso@gmail.com>
2018-06-26 16:56:33 -0300

    validação de formato de e-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
 	has_one :student, dependent: :destroy, autosave: true
 	has_one :registrant, dependent: :destroy, inverse_of: :user, autosave: true
+	validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
